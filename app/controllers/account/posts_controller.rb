ac2b7c19efbc192e5caa9a9ba9564adafc6c81a9
emilesu <emilesu@qq.com>
2017-01-09 22:16:02 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,7 +1,25 @@
 class Account::PostsController < ApplicationController
-  before_action :authenticate_user!
+
 
   def index
     @posts = current_user.posts
   end
+
+#没做出来，等第三遍练习的时候补上
+#  def edit
+#    @group = Group.find(params[:group_id])
+#    @post = Post.find(params[:id])
+#  end
+
+
+
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
 end
